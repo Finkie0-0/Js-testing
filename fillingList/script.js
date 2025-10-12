5b2566d9d9dcea34dc439cd589928b112f8c1499
Arnold Mdluli <arnoldmdluli00@gmail.com>
2025-10-12 23:53:13 +0200

    Build the basic structure of the program as well as style of the program

--- fillingList/script.js
+++ fillingList/script.js
@@ -1,1 +1,20 @@
-console.log("Hi!")
+//Make a list or array of people
+//Add the names Phil and Lola into the not invited list else add into the added list
+
+const people = [
+    "chris",
+    "Anne",
+    "Colin",
+    "Terri",
+    "Phil",
+    "Lola",
+    "Sam",
+    "Kay",
+    "Bruce",
+];
+
+// const admitted = document.querySelector(".admitted");
+// const refused = document.querySelector(".refused");
+// admitted.textContent = "Admit: ";
+// refused.textContent = "Refuse: ";
+
