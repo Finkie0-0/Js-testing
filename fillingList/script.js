f2bad8d67a01cc7025958c0db66531c00bb56929
Arnold Mdluli <arnoldmdluli00@gmail.com>
2025-12-12 15:15:17 +0200

    Try looking for a number in a list

--- fillingList/script.js
+++ fillingList/script.js
@@ -17,3 +17,32 @@
 // // const refused = document.querySelector(".refused");
 // // admitted.textContent = "Admit: ";
 // // refused.textContent = "Refuse: ";
+
+const contacts = [
+    "Chris: 22233232",
+    "Sarah: 23212313",
+    "Bill: 21123112",
+    "Mary: 23334234",
+    "Dianne: 92302913"
+];
+
+const para = document.querySelector("p");
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", () => {
+    const searchName = input.value.toLocaleLowerCase();
+    input.value = "";
+    input.focus();
+    para.textContent = "";
+    for (const contact of contacts) {
+        const splitContact = contact.split(":");
+        if (splitContact[0].toLocaleLowerCase() === searchName){
+            para.textContent = `${splitContact[0]}'s number is ${splitContact[1]}.`;
+            break;
+        };
+    };
+    if (para.textContent === ""){
+        para.textContent = "Contact not Found."
+    }
+});
