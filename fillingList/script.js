134b56345c42ac28f38b622c5c4869bbf6b49170
Arnold Mdluli <arnoldmdluli00@gmail.com>
2025-12-12 13:00:05 +0200

    Relearn DOM

--- fillingList/script.js
+++ fillingList/script.js
@@ -1,20 +1,34 @@
 //Make a list or array of people
 //Add the names Phil and Lola into the not invited list else add into the added list
 
-const people = [
-    "chris",
-    "Anne",
-    "Colin",
-    "Terri",
-    "Phil",
-    "Lola",
-    "Sam",
-    "Kay",
-    "Bruce",
-];
+// const people = [
+//     "chris",
+//     "Anne",
+//     "Colin",
+//     "Terri",
+//     "Phil",
+//     "Lola",
+//     "Sam",
+//     "Kay",
+//     "Bruce",
+// ];
 
-// const admitted = document.querySelector(".admitted");
-// const refused = document.querySelector(".refused");
-// admitted.textContent = "Admit: ";
-// refused.textContent = "Refuse: ";
+// // const admitted = document.querySelector(".admitted");
+// // const refused = document.querySelector(".refused");
+// // admitted.textContent = "Admit: ";
+// // refused.textContent = "Refuse: ";
 
+const results = document.querySelector('#results');
+
+function calculate(){
+    for (let i = 1; i < 10; i++){
+        const newResult = `${i} X ${i} = ${i * i}`;
+        results.textContent += `${newResult}\n`;
+    }
+    results.textContent += "\nFinished!\n\n";
+}
+const calculateBtn = document.querySelector('#calculate');
+const clearBtn = document.querySelector("#clear");
+
+calculateBtn.addEventListener("click", calculate);
+clearBtn.addEventListener("click", ()=>(results.textContent = ""));
