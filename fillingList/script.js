ab3bca87025cb6cd8047bf3e7b1955491eafb95e
Arnold Mdluli <arnoldmdluli00@gmail.com>
2025-12-12 16:40:13 +0200

    Use a full function to search for the contact info instead of the enmpty function

--- fillingList/script.js
+++ fillingList/script.js
@@ -18,23 +18,34 @@
 // // admitted.textContent = "Admit: ";
 // // refused.textContent = "Refuse: ";
 
+//variable with the contact infomation
+const contacts = [
+    "A:12345",
+    "B:23456",
+    "C:34567",
+    "D:45678",
+    "E:56789",
+];
 
-//Display selector
-const results = document.querySelector("#results");
+const para = document.querySelector("p");
+const input = document.querySelector("input");
+const btn = document.querySelector("button");
 
-//Calculations made then sent to the Displayer
-function calculate(){
-    for (let i = 1; i < 10; i++){
-        const newResult = `${i} x ${i} = ${i * i}`;
-        results.textContent += `${newResult}\n`;
+function Searching(){
+    const searchName = input.value.toLowerCase();
+    input.value = "";
+    input.focus();
+    para.textContent = "";
+    for (const contact of contacts){
+        const splitContact = contact.split(":");
+        if (splitContact[0].toLowerCase() === searchName){
+            para.textContent = `${splitContact[0]}'s number is ${splitContact[1]}.`;
+            break;
+        };
+    }
+    if (para.textContent === ""){
+        para.textContent = "Contact not found.";
     };
-    results.textContent += "\nFinished!\n\n";
 };
 
- //Button initializations
-const calculateBtn = document.querySelector("#calculate");
-const clearBtn = document.querySelector("#clear");
-
-//events that should happen when buttons have been clicked
-calculateBtn.addEventListener("click", calculate);
-clearBtn.addEventListener("click", ()=> (results.textContent = ""));
+btn.addEventListener("click", Searching);
