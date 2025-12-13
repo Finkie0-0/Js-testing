6e50c62c326ac876584b8881b97e2638a9a789c7
Arnold Mdluli <arnoldmdluli00@gmail.com>
2025-12-13 15:44:36 +0200

    Take a number then display the even numbers and the odd ones

---
+++ Real_evens/script.js
@@ -0,0 +1,25 @@
+const display = document.querySelector("#display");
+const input = document.querySelector("input");
+const find = document.querySelector("#find");
+const clear = document.querySelector("#clear");
+
+find.addEventListener("click", ()=>{
+    display.textContent = "";
+    const num = input.value;
+    input.value = "";
+    input.focus();
+
+    for (let i = 1; i <= num; i++){
+        if (i % 2 !== 0){
+            display.textContent += `${i} in an Odd number\n`;
+        }
+        else{
+            display.textContent += `${i} even!\n`;
+        }
+        
+    };
+    display.textContent += `\nFinished!\n`
+});
+
+
+
