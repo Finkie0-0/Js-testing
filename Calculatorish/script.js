f215176376d11fc6444260daa31f30b64721c90f
Arnold Mdluli <arnoldmdluli00@gmail.com>
2025-12-15 01:17:41 +0200

    Clear out the bloated repeat querySelector

--- Calculatorish/script.js
+++ Calculatorish/script.js
@@ -3,14 +3,7 @@
 const num1 = document.querySelector("#firstNum");
 const num2 = document.querySelector("#secondNum");
 
-const plus = document.querySelector("#plus");
-const sub = document.querySelector("#sub");
-const multi = document.querySelector("#multi");
-const div = document.querySelector("#div");
-const mod = document.querySelector("#mod");
-const clear = document.querySelector("#clear");
-
-const display = document.querySelector("pre")
+const display = document.querySelector("pre");
 
 function add(){
     display.textContent = `${num1} + ${num2} = ${num1 + num2}`;
