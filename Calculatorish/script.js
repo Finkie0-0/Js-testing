6074383cef70b925d67691c29676a02569b63620
Arnold Mdluli <arnoldmdluli00@gmail.com>
2025-12-15 01:58:59 +0200

    Work on the addition button, try to get the logic behind getting the input for the calculations

--- Calculatorish/script.js
+++ Calculatorish/script.js
@@ -1,12 +1,19 @@
 //Add declarations to the input, buttons and the display
 
-const num1 = document.querySelector("#firstNum");
-const num2 = document.querySelector("#secondNum");
+const num1 = document.getElementById("Num1");
+const num2 = document.getElementById("Num2");
 
 const display = document.querySelector("pre");
 
-function add(){
-    display.textContent = `${num1} + ${num2} = ${num1 + num2}`;
-}
+const plus = document.getElementById("plus");
+const clr = document.querySelector("#clear");
 
-plus.addEventListener("click", add);
+
+plus.addEventListener("click", ()=>{
+    const resultAdd = `${num1} + ${num2} = ${num1 + num2}`;
+
+    display.textContent = `${resultAdd}`;
+});
+
+clr.addEventListener("click", ()=> (display.textContent = ""));
+
