89d0d85a189358d60296333fcd7e5344edbd0388
Arnold Mdluli <arnoldmdluli00@gmail.com>
2025-12-15 02:16:03 +0200

    Clear everything then start everything from the ground up

--- Calculatorish/script.js
+++ Calculatorish/script.js
@@ -1,19 +1,1 @@
 //Add declarations to the input, buttons and the display
-
-const num1 = document.getElementById("Num1");
-const num2 = document.getElementById("Num2");
-
-const display = document.querySelector("pre");
-
-const plus = document.getElementById("plus");
-const clr = document.querySelector("#clear");
-
-
-plus.addEventListener("click", ()=>{
-    const resultAdd = `${num1} + ${num2} = ${num1 + num2}`;
-
-    display.textContent = `${resultAdd}`;
-});
-
-clr.addEventListener("click", ()=> (display.textContent = ""));
-
