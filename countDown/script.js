d935ee248a0a7f3387bbf7f7277ddd2701abff31
Arnold Mdluli <arnoldmdluli00@gmail.com>
2025-12-15 22:47:37 +0200

    Remove the for loop to clean up the code

--- countDown/script.js
+++ countDown/script.js
@@ -1,34 +1,19 @@
 const output = document.querySelector(".output");
 output.textContent = "";
 
-// let i = 10;
+let i = 10;
 
-// while (i >= 0){
-//     const para = document.createElement("p");
-//     if(i === 0){
-//         para.textContent = `Boom! Shaka Laka!`;
-//     }
-//     else if (i === 10){
-//         para.textContent = `Countdown ${i}!`;
-//     }
-//     else{
-//         para.textContent = `${i}`
-//     }
-//     output.appendChild(para);
-//     i--;
-// }
-
-for (let i = 10; i >= 0; i--){
-    const para = document.createElement('p');
-
+while (i >= 0){
+    const para = document.createElement("p");
     if(i === 0){
-        para.textContent = `BlastOff!`;
+        para.textContent = `Boom! Shaka Laka!`;
     }
     else if (i === 10){
-        para.textContent = `Countdown ${i}`;
+        para.textContent = `Countdown ${i}!`;
     }
     else{
-        para.textContent = `${i}`;
-    };
+        para.textContent = `${i}`
+    }
     output.appendChild(para);
+    i--;
 }
