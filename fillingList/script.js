42d48349572ea0ef68d9153dabc03fbff2cf944e
Arnold Mdluli <arnoldmdluli00@gmail.com>
2025-12-15 23:21:53 +0200

    Make two lists that has people that can be invited and people who are not admitted to the party so

--- fillingList/script.js
+++ fillingList/script.js
@@ -1,51 +1,27 @@
 //Make a list or array of people
 //Add the names Phil and Lola into the not invited list else add into the added list
 
-// const people = [
-//     "chris",
-//     "Anne",
-//     "Colin",
-//     "Terri",
-//     "Phil",
-//     "Lola",
-//     "Sam",
-//     "Kay",
-//     "Bruce",
-// ];
-
-// // const admitted = document.querySelector(".admitted");
-// // const refused = document.querySelector(".refused");
-// // admitted.textContent = "Admit: ";
-// // refused.textContent = "Refuse: ";
-
-//variable with the contact infomation
-const contacts = [
-    "A:12345",
-    "B:23456",
-    "C:34567",
-    "D:45678",
-    "E:56789",
+const people = [
+    "chris",
+    "Anne",
+    "Colin",
+    "Terri",
+    "Phil",
+    "Lola",
+    "Sam",
+    "Kay",
+    "Bruce",
 ];
 
-const para = document.querySelector("p");
-const input = document.querySelector("input");
-const btn = document.querySelector("button");
+const admitted = document.querySelector(".admitted");
+const refused = document.querySelector(".refused");
+admitted.textContent = "Admit: ";
+refused.textContent = "Refuse: ";
 
-function Searching(){
-    const searchName = input.value.toLowerCase();
-    input.value = "";
-    input.focus();
-    para.textContent = "";
-    for (const contact of contacts){
-        const splitContact = contact.split(":");
-        if (splitContact[0].toLowerCase() === searchName){
-            para.textContent = `${splitContact[0]}'s number is ${splitContact[1]}.`;
-            break;
-        };
+for (i = 0; i < people.length; i++){
+    if (people[i] === "Phil" || people[i] === "Lola"){
+        refused.textContent += `\n${people[i]}`;
+    }else{
+        admitted.textContent += `\n${people[i]}\n`;
     }
-    if (para.textContent === ""){
-        para.textContent = "Contact not found.";
-    };
-};
-
-btn.addEventListener("click", Searching);
+}
