3c2208b802da732d85bfe0006b889ac319ece1c3
Arnold Mdluli <arnoldmdluli00@gmail.com>
2025-12-16 16:56:02 +0200

    Make a program that takes user inputs then add those numbers

--- Practise/script.js
+++ Practise/script.js
@@ -1,26 +1,39 @@
-const names = [
-    "A",
-    "S",
-    "D",
-    "F",
-    "G",
-    "H",
-    "J",
-    "K",
-    "L"
-];
+// const names = [
+//     "A",
+//     "S",
+//     "D",
+//     "F",
+//     "G",
+//     "H",
+//     "J",
+//     "K",
+//     "L"
+// ];
 
-const look = document.querySelector(".look");
-const rest = document.querySelector(".rest");
+// const look = document.querySelector(".look");
+// const rest = document.querySelector(".rest");
 
-look.textContent = "Looking for: ";
-rest.textContent = "The Rest: ";
+// look.textContent = "Looking for: ";
+// rest.textContent = "The Rest: ";
 
-for (i = 0; i < names.length; i++){
-    if (names[i] === "S"){
-        look.textContent += `letter: ${names[i]} in position: ${i + 1}`
-    }else{
-        rest.textContent += `\n${names[i]}`
-    }
+// for (i = 0; i < names.length; i++){
+//     if (names[i] === "S"){
+//         look.textContent += `letter: ${names[i]} in position: ${i + 1}`
+//     }else{
+//         rest.textContent += `\n${names[i]}`
+//     }
 
+// }
+
+const sum = document.querySelector(".sum");
+const values = document.querySelector(".numbers");
+
+let added = 0
+
+while (true){
+    num = +prompt("Enter a number:", "");
+    if (!num) break;
+    added += num;
+    values.textContent += `${num}`;
 }
+sum.textContent += `The sum is ${added}`;
