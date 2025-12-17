6cb45a92f4874f80cfecee22d41aaa2f6565f480
Arnold Mdluli <arnoldmdluli00@gmail.com>
2025-12-17 19:03:27 +0200

    Test all the array methods

--- Practise/Array_practice/script.js
+++ Practise/Array_practice/script.js
@@ -1,8 +1,24 @@
 // let arr = ["A","B","C","D"];
 
 // console.log(arr[0])
-let arr = ['Apple', {name: 'Arnold'}, true, function(){alert('Hello')}];
 
-alert (arr[1].name);
+// let arr = ['Apple', {name: 'Arnold'}, true, function(){alert('Hello')}];
 
-arr[3]();
+// alert (arr[1].name);
+
+// arr[3]();
+
+let names = ["Arnold","Priscilla","Nokukhanya"];
+
+// names.push("Siya");
+
+// console.log(names);
+
+// names.pop()
+// console.log(names);
+
+// names.shift();
+// console.log(names);
+
+// names.unshift("Arnold");
+// console.log(names);
