210728c7abfe0296ecfee85cd58819100cd288a9
Arnold Mdluli <arnoldmdluli00@gmail.com>
2025-12-21 21:56:30 +0200

    Make it work without the function

--- Practise/Sum_of_tripled_Evens/script.js
+++ Practise/Sum_of_tripled_Evens/script.js
@@ -1,16 +1,13 @@
 //Take in an array
+const arr = [1,2,3,4,5,6,7,8,9,10];
 
-function sumOfTripledEvens(array){
-    let sum = 0;
-    for (let i = 0; i < array.length; i++){
-        //Step 1: If the element is an even number
-        if (array[i] % 2 === 0){
-            // Step 2: Multiply this number by three
-            const tripleEvenNumber = array[i] * 3;
-            //Step 3: Add the new number to the total
-            sum += tripleEvenNumber;
-        }
+let sum = 0;
+
+for (i = 0; i < arr.length; i++){
+    if (arr[i] % 2 === 0){
+        let tripled = arr[i] * 3;
+
+        sum += tripled;
     }
-    return sum;
 }
-
+console.log(sum);
