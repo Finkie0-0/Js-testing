7326ef52154d9cca34639a9f9f4914fb62eafeb6
Arnold Mdluli <arnoldmdluli00@gmail.com>
2025-12-21 23:45:41 +0200

    Use the arrow function

--- Practice/The_filter_method/script.js
+++ Practice/The_filter_method/script.js
@@ -1,6 +1,10 @@
-function isOdd(num){
-    return num % 2 !== 0;
-}
+// function isOdd(num){
+//     return num % 2 !== 0;
+// }
+// const arr = [1,2,3,4,5,6,7];
+// const oddNums = arr.filter(isOdd);
+// console.log(oddNums);
+
 const arr = [1,2,3,4,5,6,7];
-const oddNums = arr.filter(isOdd);
-console.log(oddNums);
+const oddNums = arr.filter((num) => num % 2 !== 0);
+console.log(oddNums)
